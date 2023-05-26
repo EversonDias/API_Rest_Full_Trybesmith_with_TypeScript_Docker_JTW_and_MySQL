b7e54a495ba69e7eedaa50af84e6fffb1b0ba5ef
Everson <eversond14s@gmail.com>
2023-05-26 14:34:10 -0300

    fixed lint

--- src/Service/productsServices.ts
+++ src/Service/productsServices.ts
@@ -1,8 +1,8 @@
-import { ProductWithoutId, SericeResponse } from 'src/types/types';
+import { ProductWithoutId, ServiceResponse } from 'src/types/types';
 import ProductModel from '../database/models/product.model';
 import status from '../utils/HTTP';
 
-const createProduct = async (product: ProductWithoutId): Promise<SericeResponse> => {
+const createProduct = async (product: ProductWithoutId): Promise<ServiceResponse> => {
   const response = await ProductModel.create(product);
   return { status: status.CREATED, data: response.dataValues };
 };
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,13 +1,13 @@
 import { Product } from './Product';
 
-export type ProductWithoutId = Omit<Product, 'id'>
+export type ProductWithoutId = Omit<Product, 'id'>;
 export type BodyProduct = {
   name: string;
   price: string;
   orderId: number;
 };
 export type ProductWithoutOrderId = Omit<Product, 'orderId'>;
-export type SericeResponse = {
+export type ServiceResponse = {
   status: number;
   data: ProductWithoutOrderId;
 };
